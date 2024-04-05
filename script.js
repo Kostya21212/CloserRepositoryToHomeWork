2a653042690cc36f9d8fecd06f58b3db12d50f59
Onischenko Kostyantyn <Onischenko411@gmail.com>
2024-04-06 00:20:14 +0300

    File add

---
+++ script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+
+function createCountCloser (){
+    let count = 0;
+    const myFunctionCloser = function (num){
+        count = count + num;
+        return count;
+    }
+    return myFunctionCloser
+}
+
+const increment = createCountCloser();
+const numberCloserOne = increment(3);
+const numberCloserTwo= increment(5);
+const numberCloserThree = increment(20);
+
+
+console.log(numberCloserOne,numberCloserTwo,numberCloserThree);
+
+
+
+
+
+
+
+
+
+
+// const box = document.querySelector('.box');
+
+// const newHeight = 110;
+// const newWidth = 400;
+
+// function changeParams(elem, h, w) {
+//     elem.style.height = `${h ?? 200}px`;
+//     elem.style.width = `${w ?? 200}px`;
+//     elem.innerHTML = ( h ?? 200 )* (w ?? 200);
+// }
+
+// changeParams(box, newHeight, newWidth);
+
+
+// let userName ;
+// let userKey ;
+// console.log(userName ?? userKey ??  'user')
+
+
+
+
